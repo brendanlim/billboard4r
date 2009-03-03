2062d2ffcc123e3f58a78e5a8b65517c18e71d6f
Brendan G. Lim <brendan@intridea.com>
2009-03-03 01:55:42 -0500

    Initial commit to billboard4r.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'billboard4r'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
